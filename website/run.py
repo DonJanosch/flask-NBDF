ab98ed1b9bbe31deb6a4bdb719edd259b0e00478
Jan Macenka <=>
2019-02-26 00:13:46 +0100

    Changed docker-compose.yml and .env to use the correct entry-point for SocketIO

--- website/run.py
+++ website/run.py
@@ -2,4 +2,4 @@ from website import socketio, app
 
 if __name__ == "__main__":
     #app.run(host='0.0.0.0',port=5000,debug=True)
-    socketio.run(app, debug=True, host='0.0.0.0',port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
